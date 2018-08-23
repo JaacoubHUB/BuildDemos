c3ed27493b471a809aa46df362e96b1a9a6d3686
JaacoubHUB <42650419+JaacoubHUB@users.noreply.github.com>
2018-08-23 13:01:06 -0400

    Update AccountModels.cs

--- MvcMusicStore/Models/AccountModels.cs
+++ MvcMusicStore/Models/AccountModels.cs
@@ -14,6 +14,8 @@ namespace Mvc3ToolsUpdateWeb_Default.Models
         [DataType(DataType.Password)]
         [Display(Name = "Current password")]
         public string OldPassword { get; set; }
+        
+        //test
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
